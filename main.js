1cc84b3acc2b5b7f969a8e116b94c50a98e3bcf0
TarteelAK <tarteelakram4@gmail.com>
2023-12-23 18:04:04 +0200

    first task js

---
+++ main.js
@@ -0,0 +1,4 @@
+document.querySelector(".user");
+console.log(document.querySelector(".user"));
+document.querySelector(".user").classList.add("demo");
+document.querySelector(".vip").classList.remove("vip");
